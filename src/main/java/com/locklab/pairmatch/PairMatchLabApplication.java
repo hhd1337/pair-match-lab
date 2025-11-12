ff09d2e8f0c092635af7ef124a511880149ae293
goeka <goeka1337@gmail.com>
2025-11-12 20:31:22 +0900

    init(project): Spring Initializr 기반 프로젝트 스캐폴드 추가

---
+++ src/main/java/com/locklab/pairmatch/PairMatchLabApplication.java
@@ -0,0 +1,13 @@
+package com.locklab.pairmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairMatchLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairMatchLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locklab/pairmatch/PairMatchLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locklab.pairmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairMatchLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
