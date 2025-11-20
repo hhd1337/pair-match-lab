230a9b6118bf252263a856c253343474ac477a29
goeka <goeka1337@gmail.com>
2025-11-20 12:33:22 +0900

    docs(test): 공통 응답 확인용 Test API 스웨거 문서 보완

--- src/main/java/com/locklab/pairmatch/test/controller/TestController.java
+++ src/main/java/com/locklab/pairmatch/test/controller/TestController.java
@@ -3,6 +3,7 @@ package com.locklab.pairmatch.test.controller;
 import com.locklab.pairmatch.common.response.ApiResponse;
 import com.locklab.pairmatch.test.converter.TestConverter;
 import com.locklab.pairmatch.test.dto.TestResponseDTO.TestDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 @RequiredArgsConstructor
 public class TestController {
+    @Operation(
+            summary = "응답 포맷 통일 테스트",
+            description = "응답 본문이 isSuccess, code, message, result 구조로 일관되게 반환되는지 확인하기 위한 테스트용 API입니다."
+    )
     @GetMapping("")
     public ApiResponse<TestDTO> test() {
         return ApiResponse.onSuccess(TestConverter.toTempTestDTO());
     }
-
 }
