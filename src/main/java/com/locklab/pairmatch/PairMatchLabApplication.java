745248ba254063514d2ede0e50934445b8754171
goeka <goeka1337@gmail.com>
2025-11-21 14:59:48 +0900

    feat(entity): BaseEntity 추가 및 JPA Auditing 설정

--- src/main/java/com/locklab/pairmatch/PairMatchLabApplication.java
+++ src/main/java/com/locklab/pairmatch/PairMatchLabApplication.java
@@ -2,12 +2,14 @@ package com.locklab.pairmatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PairMatchLabApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PairMatchLabApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PairMatchLabApplication.class, args);
+    }
 
 }
