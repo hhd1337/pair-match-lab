6111be43523053fde0567b8d0c9d9c53cb478236
goeka <goeka1337@gmail.com>
2025-11-22 21:31:53 +0900

    feat(repository): 매칭되지 않은 크루 조회 기능 추가

--- src/main/java/com/locklab/pairmatch/repository/CrewRepository.java
+++ src/main/java/com/locklab/pairmatch/repository/CrewRepository.java
@@ -1,8 +1,10 @@
 package com.locklab.pairmatch.repository;
 
 import com.locklab.pairmatch.entity.Crew;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CrewRepository extends JpaRepository<Crew, Long> {
+    List<Crew> findAllByMatchedFalse();
 }
 
